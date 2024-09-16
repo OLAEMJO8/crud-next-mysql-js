217654ec88d2af21fae9fb4aa20e1938b59b5bd7
OLAEMJO8 <olaecheamariajose@gmail.com>
2024-09-16 02:11:50 -0300

    feat. ProductCard ProductForm

--- src/app/components/Productform.jsx
+++ src/app/components/Productform.jsx
@@ -1,16 +1,19 @@
-'use client'
-import { useState } from "react";
+"use client";
+import { useRef, useState, useEffect } from "react";
 import axios from "axios";
-
+import { useRouter, useParams } from "next/navigation";
 
 function ProductForm() {
     const [product, setProduct] = useState({
-        name: '',
+        name: "",
         price: 0,
-        description: '',
+        description: "",
     });
 
-    //Guardar cambios en el estado
+    const form = useRef(null);
+    const router = useRouter();
+
+
     const handleChange = (e) => {
         setProduct({
             ...product,
@@ -18,35 +21,74 @@ function ProductForm() {
         });
     };
 
-    //Enviar datos por el formulario
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         const res = await axios.post("/api/products", product)
-        console.log(res)
         form.current.reset()
+        router.refresh();
+        router.push("/products");
     }
 
     return (
         <div className="flex ">
-
-            <form className="bg-white shadow-md rounded-md px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}
-                
+            <form
+                className="bg-white shadow-md rounded-md px-8 pt-6 pb-8 mb-4"
+                onSubmit={handleSubmit}
+                ref={form}
             >
+                <label
+                    htmlFor="name"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                    Product Name:
+                </label>
+                <input
+                    name="name"
+                    type="text"
+                    placeholder="name"
+                    onChange={handleChange}
+                    value={product.name}
+                    className="shadow appearance-none border rounded w-full py-2 px-3"
+                    autoFocus
+                />
 
-                <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2"> Product name</label>
-                <input name='name' type="text" placeholder="name" onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3"
-                    autoFocus />
+                <label
+                    htmlFor="price"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                    Product Price:
+                </label>
+                <input
+                    name="price"
+                    type="text"
+                    placeholder="00.00"
+                    onChange={handleChange}
+                    value={product.price}
+                    className="shadow appearance-none border rounded w-full py-2 px-3"
+                />
 
-                <label htmlFor="price" className="block text-gray-700 text-sm font-bold mb-2"> Product price</label>
-                <input name='price' type="text" placeholder="00.00" onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3"
-                    autoFocus />
+                <label
+                    htmlFor="name"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                    Product Description:
+                </label>
+                <textarea
+                    name="description"
+                    rows={3}
+                    placeholder="description"
+                    onChange={handleChange}
+                    value={product.description}
+                    className="shadow appearance-none border rounded w-full py-2 px-3"
+                />
 
-                <label htmlFor="description" className="block text-gray-700 text-sm font-bold mb-2"> Product description</label>
-                <textarea name='description' rows={3} type="text" placeholder="description" onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3"
-                    autoFocus />
+
+
+
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Crear
+                    Create Product
                 </button>
             </form>
         </div>
---
+++ src/app/products/page.jsx
@@ -0,0 +1,20 @@
+import { conn } from "@/libs/mysql";
+import ProductCard from "../components/ProductCard";
+async function loadProducts() {
+    const products = await conn.query('SELECT * FROM product')
+    return products
+  }
+
+  export const dynamic = 'force-dynamic'
+  async function ProductsPage() {
+    const products = await loadProducts();
+  
+    return <div className="grid gap-4 grid-cols-4">
+      {products.map(product => (
+          <ProductCard product={product} key={product.id} />
+      ))}
+    </div>;
+  }
+  
+
+export default ProductsPage;
