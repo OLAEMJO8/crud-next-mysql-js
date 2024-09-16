7d0deb5325db5618a63e23371d9dcee928d6e3ef
OLAEMJO8 <olaecheamariajose@gmail.com>
2024-09-16 00:55:21 -0300

    edit UPDATE

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -17,7 +17,7 @@ export async function GET(request, { params }) {
 
 export async function DELETE(request, { params }) {
     try {
-        const result = await conn.query('DELETE FROM product WHERE id = ?', [params.id ,])
+        const result = await conn.query('DELETE FROM product WHERE id = ?', [params.id,])
         if (result.affetedRows === 0) {
             return NextResponse.json({ message: 'Producto no encontrado' }, { status: 404, })
         }
@@ -29,6 +29,18 @@ export async function DELETE(request, { params }) {
     }
 }
 
-export function PUT() {
-    return NextResponse.json('actualizando producto')
+export async function PUT(request, { params }) {
+    try {
+        const data = await request.json()
+        const result = await conn.query('UPDATE product SET ? WHERE id = ?', [data, params.id])
+        if (result.affetedRows === 0) {
+            return NextResponse.json({ message: 'Producto no encontrado' }, { status: 404, })
+        }
+        const updateProduct = await conn.query('SELECT * FROM product WHERE id = ?', [params.id])
+        return NextResponse.json(updateProduct[0])
+    } catch (error) {
+        return NextResponse.json({
+            message: message.error
+        }, { status: 500, })
+    }
 }
