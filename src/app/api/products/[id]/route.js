983e4596d38a2241ec77f533e2a795943e1935c9
OLAEMJO8 <olaecheamariajose@gmail.com>
2024-09-16 00:40:07 -0300

    Edit GET for ID

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,6 +1,17 @@
 import { NextResponse } from "next/server"
-export function GET (){
-    return NextResponse.json('obteniendo un producto')
+import { conn } from "@/libs/mysql"
+export async function GET (request,  {params}){
+
+  try {
+    const result = await conn.query('SELECT * FROM product WHERE id = ?' , [params.id])
+    if (result.length === 0) {
+        return NextResponse.json({message: 'Producto no encontrado'}, {status: 404,})
+    }
+    return NextResponse.json(result[0])
+  } catch (error) {
+    return NextResponse.json({message: message.error
+    }, {status: 500,})
+  }
 }
 
 export function DELETE (){
