fefba5227a20ca1e1e250d3719af994f6a13ca68
OLAEMJO8 <olaecheamariajose@gmail.com>
2024-09-16 00:31:19 -0300

    Edit. get all product

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,8 +1,18 @@
 import { NextResponse } from "next/server";
 import { conn } from "@/libs/mysql";
 
-export function GET() {
-    return NextResponse.json('listando productos')
+export async function GET() {
+    try {
+        const results = await conn.query('SELECT * FROM product')
+
+        return NextResponse.json(results)
+    } catch (error) {
+        return NextResponse.json({
+            message: error.message,
+        }
+            ,
+            { status: 500, })
+    }
 }
 
 export async function POST(resquest) {
