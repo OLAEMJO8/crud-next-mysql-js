d51c3beea21a43bd8484b965b7b086ab2d291566
OLAEMJO8 <olaecheamariajose@gmail.com>
2024-09-15 22:54:22 -0300

    Feat. configuraciones iniciales api + routes

---
+++ src/app/api/product/[id]/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server"
+export function GET (){
+    return NextResponse.json('obteniendo un producto')
+}
+
+export function DELETE (){
+    return NextResponse.json('eliminando producto')
+}
+
+export function PUT (){
+    return NextResponse.json('actualizando producto')
+}
